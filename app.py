8e4254260e712747451a2a4563b47f1c2a2375f8
Nafiul Araf <araf.cuet16@gmail.com>
2025-04-27 21:32:08 +0600

    updated the app.py file

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import math
 import streamlit as st
 
 
-circ = round(math.pi * r ** 2, 2) # 63.62
+circ = round(math.pi * radius ** 2, 2) # 63.62
 
-triangle = round(0.5 * b * h, 2) # 10.0
+triangle = round(0.5 * base * height, 2) # 10.0
 
-ractangle = round(l * wi, 2) # 4.68
+ractangle = round(l * width, 2) # 4.68
