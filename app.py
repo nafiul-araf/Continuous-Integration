0be2450d1604c8e8339afdcb33bdcf4e902ef605
nafaraf <cuetnafaraf@gmail.com>
2025-04-27 17:19:04 +0600

    ..

---
+++ app.py
@@ -0,0 +1,14 @@
+# area calculator
+
+# area of a circle
+# area of a ractangle
+# area of triangle
+import math
+import streamlit as st
+
+
+circ = round(math.pi * r ** 2, 2) # 63.62
+
+triangle = round(0.5 * b * h, 2) # 10.0
+
+ractangle = round(l * wi, 2) # 4.68
